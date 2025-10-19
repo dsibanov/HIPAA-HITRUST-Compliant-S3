61234a0757a378568f2350059ce8885403ef150c
dsibanov <hg5k9d8md8@privaterelay.appleid.com>
2025-10-19 10:11:11 -0400

    Update variables.tf

--- modules/S3_compliance/variables.tf
+++ modules/S3_compliance/variables.tf
@@ -1,1 +1,36 @@
+# ----------------------------------------------------------------------------------
+# MODULE INPUT VARIABLES
+# These variables receive values passed from the root 'main.tf' configuration.
+# ----------------------------------------------------------------------------------
+
+variable "project_name" {
+  description = "Project name tag/prefix."
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment tag (dev, staging, prod)."
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region where resources are deployed."
+  type        = string
+}
+
+variable "retention_period_days" {
+  description = "The minimum retention period (in days) for Object Lock (HIPAA Integrity control)."
+  type        = number
+}
+
+variable "logging_bucket_retention_days" {
+  description = "The number of days to retain access logs."
+  type        = number
+}
+
+# CRITICAL INPUT: ARN of the Customer Managed Key (CMK) created in the KMS module.
+variable "kms_key_arn" {
+  description = "The ARN of the KMS CMK used for default bucket encryption."
+  type        = string
+}
 
